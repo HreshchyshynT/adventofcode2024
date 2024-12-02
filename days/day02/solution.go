7930174f76d2c4320303be4a46df5b837d017d17
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2024-12-02 10:43:06 +0200

    day 2 part 1

--- days/day02/solution.go
+++ days/day02/solution.go
@@ -6,16 +6,47 @@ import (
 )
 
 func Solve(input []string) {
+	nums := parseInput(input)
+
+	log.Printf("part 1 solution: %d\n", part1(nums))
+	log.Printf("part 2 solution: %d\n", part2(nums))
+}
+
+func part1(input [][]int) int {
+	var result int
+	for _, nums := range input {
+		check := func(left, right int) bool {
+			return right >= left || left-right > 3
+		}
+		if nums[0] < nums[1] {
+			check = func(left, right int) bool {
+				return left >= right || right-left > 3
+			}
+		}
+		result++
+
+		prev := nums[0]
+		for _, n := range nums[1:] {
+			if check(prev, n) {
+				result--
+				break
+			}
+			prev = n
+		}
+	}
+	return result
+}
+
+func part2(_ [][]int) int {
+	return 0
+}
+
+func parseInput(input []string) [][]int {
 	nums := make([][]int, len(input))
 
 	for i, line := range input {
 		nums[i] = utils.StringToIntArray(line)
 	}
 
-	part1(nums)
-}
-
-func part1(input [][]int) {
-	var result int
-	log.Printf("part 1 solution: %d\n", result)
+	return nums
 }
