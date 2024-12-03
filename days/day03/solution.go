5203960c125fe25ad772de83c4dae77fd42ee06f
Taras Hreshchyshyn <hreshchyshyn.t@gmail.com>
2024-12-03 11:13:04 +0200

    day 3 part 2

--- days/day03/solution.go
+++ days/day03/solution.go
@@ -8,37 +8,73 @@ import (
 
 func Solve(input []string) {
 	log.Printf("day 3 part 1: %d", part1(input))
+	log.Printf("day 3 part 2: %d", part2(input))
 }
 
 func part1(input []string) int {
 	var result int
 
-	for _, line := range input {
-		muls, err := findMuls(line)
-		if err != nil {
-			log.Fatalf("can't create regexp: %v\n", err)
-		}
+	muls, err := findMuls(input, true)
+	if err != nil {
+		log.Fatalf("can't create regexp: %v\n", err)
+	}
 
-		nums, err := mapMuls(muls)
-		if err != nil {
-			log.Fatalf("can't map muls: %v\n", err)
-		}
+	nums, err := mapMuls(muls)
+	if err != nil {
+		log.Fatalf("can't map muls: %v\n", err)
+	}
 
-		for _, arr := range nums {
-			result += arr[0] * arr[1]
-		}
+	for _, arr := range nums {
+		result += arr[0] * arr[1]
 	}
 
 	return result
 }
 
-func findMuls(input string) ([]string, error) {
-	r, err := regexp.Compile("mul\\([\\d+]*[\\,][\\d+]*\\)")
+// 97836217 - to hight
+func part2(input []string) int {
+	var result int
+
+	muls, err := findMuls(input, false)
+	if err != nil {
+		log.Fatalf("can't create regexp: %v\n", err)
+	}
+
+	nums, err := mapMuls(muls)
+	if err != nil {
+		log.Fatalf("can't map muls: %v\n", err)
+	}
+
+	for _, arr := range nums {
+		result += arr[0] * arr[1]
+	}
+
+	return result
+}
+
+func findMuls(input []string, alwaysEnable bool) ([]string, error) {
+	r, err := regexp.Compile("don't\\(\\)|do\\(\\)|mul\\([\\d+]*[\\,][\\d+]*\\)")
 	if err != nil {
 		return nil, err
 	}
 
-	return r.FindAllString(input, -1), nil
+	isEnabled := true
+	muls := make([]string, 0)
+	for _, line := range input {
+
+		all := r.FindAllString(line, -1)
+		for _, match := range all {
+			if match == "don't()" {
+				isEnabled = false
+			} else if match == "do()" {
+				isEnabled = true
+			} else if alwaysEnable || isEnabled {
+				muls = append(muls, match)
+			}
+		}
+	}
+
+	return muls, nil
 }
 
 func mapMuls(muls []string) ([][]int, error) {
--- days/day03/solution_test.go
+++ days/day03/solution_test.go
@@ -6,14 +6,26 @@ import (
 )
 
 func TestExample(t *testing.T) {
-	t.Run("find muls", func(t *testing.T) {
+	t.Run("find muls always enabled", func(t *testing.T) {
 		want := []string{
 			"mul(2,42222222)",
 			"mul(5,5)",
 			"mul(11,8)",
 			"mul(8,5)",
 		}
-		got, _ := findMuls("xmul(2,42222222)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))      mul ( 2 , 4 ), mul(1,2,2)")
+		got, _ := findMuls([]string{"xmul(2,42222222)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))      mul ( 2 , 4 ), mul(1,2,2)"}, true)
+
+		if !slices.Equal(want, got) {
+			t.Errorf("want %#v got %#v", want, got)
+		}
+	})
+
+	t.Run("find muls not always enabled", func(t *testing.T) {
+		want := []string{
+			"mul(2,4)",
+			"mul(8,5)",
+		}
+		got, _ := findMuls([]string{"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"}, false)
 
 		if !slices.Equal(want, got) {
 			t.Errorf("want %#v got %#v", want, got)
@@ -43,6 +55,14 @@ func TestExample(t *testing.T) {
 	t.Run("part1", func(t *testing.T) {
 		want := 161
 		got := part1([]string{"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"})
+
+		if want != got {
+			t.Errorf("want %d got %d", want, got)
+		}
+	})
+	t.Run("part2", func(t *testing.T) {
+		want := 48
+		got := part2([]string{"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))don't()mul(1,2))"})
 
 		if want != got {
 			t.Errorf("want %d got %d", want, got)
